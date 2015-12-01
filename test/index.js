146e3282144e97344dbd37684370cf3a39f8309f
Mohammed AL-AGEL <mohammed_alagel@yahoo.com>
2015-12-01 16:25:56 +0100

    resolve npm package ??? problem

--- test/index.js
+++ test/index.js
@@ -1,8 +1,12 @@
 import assert from 'assert';
+import { should } from 'chai';
 import circle from '../lib';
+
+should(); // Modifies objects prototype to include the 'should' property
 
 describe('catatonic-circle', function () {
   it('should have a version number!', function () {
     assert(typeof circle.VERSION !== 'undefined', 'The Project should have a VERSION, whatever the actual version.');
   });
 });
+
